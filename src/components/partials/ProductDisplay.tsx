1fafa259a0822bd906e0a486df4996f772a4f381
wayungi <fwayungi.com>
2023-10-10 08:37:50 +0300

    ADD: productDsiplay for dipalying a list of products

---
+++ src/components/partials/ProductDisplay.tsx
@@ -0,0 +1,22 @@
+import ProductModel from "../../Model/Product"; 
+import ProductPanel from "./ProductPanel";
+
+type ProductDisplayProps =  {
+    productList: ProductModel[]
+}
+
+const ProductDisplay = ({ productList }:ProductDisplayProps) =>  {
+
+    let productDsiplayList  = null
+    if(productList.length > 0){
+        productDsiplayList = productList.map((product) => ProductPanel(product))
+    }
+
+    return (
+        <section>
+           {productDsiplayList? productDsiplayList: "No products avaialable"}
+        </section>
+    );
+};
+
+export default ProductDisplay;
