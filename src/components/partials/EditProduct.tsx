f8f6b25e7ebce645f5007511489b3be7c8a46562
wayungi <fwayungi.com>
2023-10-12 12:40:52 +0300

    ADD: edit prodcut feature

--- src/components/partials/EditProduct.tsx
+++ src/components/partials/EditProduct.tsx
@@ -1,8 +1,9 @@
-// import { ChangeEvent, useState } from 'react';
-// import { useAppSelector } from '../../app/hooks';
-import {/*AiOutlineSave,*/ AiFillCloseCircle} from 'react-icons/ai';
+import { ChangeEvent, useState } from 'react';
+import { useAppSelector, useAppDispatch  } from '../../app/hooks';
+import {AiOutlineSave, AiFillCloseCircle} from 'react-icons/ai';
 import Product from '../../Model/Product';
 import { SetStateAction } from 'react';
+import { editProduct } from '../../features/products/productSlice';
 
 type editProductProps =  { 
     product: Product, 
@@ -10,20 +11,32 @@ type editProductProps =  {
 }
 
 const EditProduct = ({product, setIsEditable}: editProductProps) => {
-    console.log(product)
-    // const {id, name, image, price, category, stock} = product;
-    // const categories = useAppSelector((state) => state.products.categories);
-    // const [productName, setProductName] = useState<string>(name);
-    // const [productPrice, setProductPrice] = useState<Number>(price);
-    // const [stockQuantity, setStockQuantity] = useState<Number>(stock);
+    const dispatch = useAppDispatch();
+
+    const {id, name, image, price, category, stock } = product;
+    const categories = useAppSelector((state) => state.products.categories);
+    const [productName, setProductName] = useState<string>(name);
+    const [productPrice, setProductPrice] = useState<Number>(price);
+    const [stockQuantity, setStockQuantity] = useState<Number>(stock);
 
     const handleCloseModal = () => setIsEditable(false);
-    // const handleSaveEdits = () => {}
+    const handleSaveEdits = () => {
+        const updateProduct = {
+            id,
+            "name": productName,
+            "image": image, //update this to be able to update image
+            "price": productPrice,
+            category, // this to will need to be updare
+            "stock": stockQuantity
+        }
+        dispatch(editProduct(updateProduct));
+        setIsEditable(false);
+    }
 
-    // // handle form field updates
-    // const handleProductNameUpdate = (event: ChangeEvent<HTMLInputElement>) => setProductName(event.target.value);
-    // const handlePriceUpdate = (event: ChangeEvent<HTMLInputElement>) => setProductPrice(+event.target.value);
-    // const handleStockUpdate = (event: ChangeEvent<HTMLInputElement>) => setStockQuantity(+event.target.value);
+    // handle form field updates
+    const handleProductNameUpdate = (event: ChangeEvent<HTMLInputElement>) => setProductName(event.target.value);
+    const handlePriceUpdate = (event: ChangeEvent<HTMLInputElement>) => setProductPrice(+event.target.value);
+    const handleStockUpdate = (event: ChangeEvent<HTMLInputElement>) => setStockQuantity(+event.target.value);
 
     return (
         <div className="overlay">
@@ -34,34 +47,34 @@ const EditProduct = ({product, setIsEditable}: editProductProps) => {
                     <AiFillCloseCircle size="2em" onClick={() => handleCloseModal()}/>
                 </div>
 
-                {/* <label htmlFor="id">Product Id
+                <label htmlFor="id">Product Id
                     <input type="text" value={id} contentEditable="false" id="id" readOnly/>
-                </label> */}
+                </label>
 
-                {/* <label htmlFor="name">Product Name
+                <label htmlFor="name">Product Name
                     <input type="text" value={productName} name="name" id="name" onChange={(e) => handleProductNameUpdate(e)}/>
-                </label> */}
+                </label>
 
-                {/* <div>
+                <div>
                     <img  src={image} alt={name} />
-                </div> */}
+                </div>
 
-                {/* <label htmlFor="price">Price
+                <label htmlFor="price">Price
                     <input type="text" value={productPrice.toString()}  id="price" onChange={(e) => handlePriceUpdate(e)}/>
-                </label> */}
+                </label>
 
-                {/* <label htmlFor="catgeory">Category
-                    <select>
+                <label htmlFor="catgeory">Category
+                    <select name="selectName">
                         <option value={category}>{category}</option>
-                        {categories.map((category) => <option value={category}>{category}</option>)}
+                        {categories.map((category, index) => <option key={index} value={category}>{category}</option>)}
                         </select>
-                </label> */}
+                </label>
 
-                {/* <label htmlFor="stock">Quantity in stock
+                <label htmlFor="stock">Quantity in stock
                     <input type="text" value={stockQuantity.toString()} onChange={(e) => handleStockUpdate(e)} />
-                </label> */}
+                </label>
 
-                {/* <AiOutlineSave size="2em" onClick={() => handleSaveEdits()}/> */}
+                <AiOutlineSave size="2em" onClick={() => handleSaveEdits()}/>
 
             </div> 
         </div>
