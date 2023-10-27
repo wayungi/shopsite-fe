9daa4a7305c27417943e16f66547b1b39ea38f50
wayungi <fwayungi.com>
2023-10-27 12:47:50 +0300

    ADD: admin-nav styles

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import {CiDeliveryTruck} from 'react-icons/ci';
+import {GoPackageDependencies, GoPackageDependents} from 'react-icons/go';
+import { BiStore } from 'react-icons/bi'
+import {FaShopify} from 'react-icons/fa';
+
+
+const NavBar = () => {
+  return (
+    <aside className="admin-nav">
+        <ul className="admin-menu-items">
+            <li className="admin-list-items">Home <FaShopify size="2em"/></li>
+            <li className="admin-list-items">New product <CiDeliveryTruck size="2em"/></li>
+            <li>Product Managements <BiStore size="2em"/></li>
+            <li>View Orders <GoPackageDependencies size="2em" /></li> {/* can cancel orders too*/}
+            <li>View dispatched Orders <GoPackageDependents size="2em" /></li>
+        </ul>
+    </aside>
+  )
+}
+
+export default NavBar
