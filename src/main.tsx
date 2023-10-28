468e3eec66579e710308db8fd761ec407c10015b
wayungi <fwayungi.com>
2023-10-28 08:42:09 +0300

    ADD: error page

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,23 @@ import './index.css'
 import { Provider } from 'react-redux';
 import { store } from './app/store.tsx';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:  <App />, //root route, all other routes will render inside of it, it is also the root layout
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <RouterProvider router={router} />
+      {/* <App /> */}
     </Provider>
   </React.StrictMode>,
 )
