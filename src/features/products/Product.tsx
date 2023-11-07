ac7aea46bf3005e0787fe8ddca8f6355c9aeee20
wayungi <fwayungi.com>
2023-11-07 13:32:26 +0300

    REMOVE: log statements, add comments for later refactor

--- src/features/products/Product.tsx
+++ src/features/products/Product.tsx
@@ -7,14 +7,14 @@ import { useOutletContext } from "react-router-dom";
 const Product = () => {
   const status = useAppSelector(state => state.products.status);
   const products =  useAppSelector(state => selectProducts(state));
-  let content: ReactNode = '';
+  let content: (ReactNode | null) = null;
   const filterValue: string = useOutletContext();
-  console.log(filterValue)
 
   // refactor this into another component
   if(status === "succeeded") {
     const filteredProducts = products.filter((product) => product.name.includes(filterValue)|| product.category.includes(filterValue));
     content = filteredProducts.map((product) => (
+      // Refactor this artcile into a component
       <article className="product-panel" key={product._id} >
         <div className="item-image">
           <img src={product.image} alt={product.name} />
